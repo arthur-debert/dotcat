716f31137a8862915d6a3ba2d00c6051735f4a73
adebert <debert@gmail.com>
2024-12-20 19:52:27 -0300

    refactor test functions to remove monkeypatching and simplify argument handling

--- tests/test_json.py
+++ tests/test_json.py
@@ -21,44 +21,38 @@ def empty_json_file(tmp_path):
     file_path.write_text('{}')
     return file_path
 
-def test_json_parsing(monkeypatch, json_file):
-    test_args = ['dotcat', str(json_file), 'python.editor.tabSize']
-    monkeypatch.setattr('sys.argv', test_args)
+def test_json_parsing(json_file):
+    test_args = [str(json_file), 'python.editor.tabSize']
     captured_output = StringIO()
     sys.stdout = captured_output
-    run(test_args[1:])
+    run(test_args)
     sys.stdout = sys.__stdout__
     assert captured_output.getvalue().strip() == '4'
 
-def test_json_parsing_no_key(monkeypatch, json_file):
-    test_args = ['dotcat', str(json_file), 'python.editor.dictator']
-    monkeypatch.setattr('sys.argv', test_args)
-    captured_output = StringIO()
+def test_json_parsing_no_key(json_file):
+    test_args = [str(json_file), 'python.editor.dictator']
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        run(test_args[1:])
+        run(test_args)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
 
-def test_json_key_error(monkeypatch, json_file):
-    test_args = ['dotcat', str(json_file), 'python.editor.unknownKey']
-    monkeypatch.setattr('sys.argv', test_args)
+def test_json_key_error(json_file):
+    test_args = [str(json_file), 'python.editor.unknownKey']
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        run(test_args[1:])
+        run(test_args)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
 
-def test_malformed_json(monkeypatch, malformed_json_file):
-    test_args = ['dotcat', str(malformed_json_file), 'python.editor.tabSize']
-    monkeypatch.setattr('sys.argv', test_args)
+def test_malformed_json(malformed_json_file):
+    test_args = [str(malformed_json_file), 'python.editor.tabSize']
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        run(test_args[1:])
+        run(test_args)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
 
-def test_empty_json(monkeypatch, empty_json_file):
-    test_args = ['dotcat', str(empty_json_file), 'python.editor.tabSize']
-    monkeypatch.setattr('sys.argv', test_args)
+def test_empty_json(empty_json_file):
+    test_args = [str(empty_json_file), 'python.editor.tabSize']
     with pytest.raises(SystemExit) as pytest_wrapped_e:
-        run(test_args[1:])
+        run(test_args)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
