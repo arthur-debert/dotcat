63649eef06fb53c2008ecba0cb8c62e93e733bcf
adebert <debert@gmail.com>
2024-12-20 20:02:55 -0300

    add test fixtures for empty and malformed TOML/YAML files and update test functions

--- tests/test_toml.py
+++ tests/test_toml.py
@@ -4,48 +4,48 @@ import sys
 from io import StringIO
 
 @pytest.fixture
-def json_file():
-    return "tests/fixtures/test.json"
+def toml_file():
+    return "tests/fixtures/test.toml"
 
 @pytest.fixture
-def malformed_json_file():
-    return "tests/fixtures/malformed.json"
+def malformed_toml_file():
+    return "tests/fixtures/malformed.toml"
 
 @pytest.fixture
-def empty_json_file():
-    return "tests/fixtures/empty.json"
+def empty_toml_file():
+    return "tests/fixtures/empty.toml"
 
-def test_json_parsing(json_file):
-    test_args = [json_file, 'python.editor.tabSize']
+def test_toml_parsing(toml_file):
+    test_args = [toml_file, 'python.editor.tabSize']
     captured_output = StringIO()
     sys.stdout = captured_output
     run(test_args)
     sys.stdout = sys.__stdout__
     assert captured_output.getvalue().strip() == '4'
 
-def test_json_parsing_no_key(json_file):
-    test_args = [json_file, 'python.editor.dictator']
+def test_toml_parsing_no_key(toml_file):
+    test_args = [toml_file, 'python.editor.dictator']
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         run(test_args)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
 
-def test_json_key_error(json_file):
-    test_args = [json_file, 'python.editor.unknownKey']
+def test_toml_key_error(toml_file):
+    test_args = [toml_file, 'python.editor.unknownKey']
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         run(test_args)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
 
-def test_malformed_json(malformed_json_file):
-    test_args = [malformed_json_file, 'python.editor.tabSize']
+def test_malformed_toml(malformed_toml_file):
+    test_args = [malformed_toml_file, 'python.editor.tabSize']
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         run(test_args)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 1
 
-def test_empty_json(empty_json_file):
-    test_args = [empty_json_file, 'python.editor.tabSize']
+def test_empty_toml(empty_toml_file):
+    test_args = [empty_toml_file, 'python.editor.tabSize']
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         run(test_args)
     assert pytest_wrapped_e.type == SystemExit
