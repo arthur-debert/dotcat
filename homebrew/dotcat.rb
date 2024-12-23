4f4c02d517eb6a1bf3f6d8787f5711280b17e641
adebert <debert@gmail.com>
2024-12-23 17:44:09 -0300

    feat: add GitHub Actions workflow to update Homebrew formula and restructure formula files

--- homebrew/dotcat.rb
+++ homebrew/dotcat.rb
@@ -1,3 +1,13 @@
+class Dotcat < Formula
+  include Language::Python::Virtualenv  # <--- Add this line
+  depends_on "python@3.9"
+  depends_on "poetry"
+
+  desc "Cat structured data, in style"
+  homepage "https://github.com/arthur-debert/dotcat"
+
+    url "https://files.pythonhosted.org/packages/69/1d/8743e6c1b833005ef757f3c61f1ba166571f932830b92793629e60e2bcef/dotcat-0.8.2.tar.gz"
+    sha256 "d8049b66e9afe5914ce9ee255de9f9f3e8dbe8dae1bb548d1df32a3317865d3b"
 
   def install
     virtualenv_path = libexec / "venv"
