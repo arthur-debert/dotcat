8b14834e27a5e60dcbf1b1d8294119c0ea8eb75c
adebert <debert@gmail.com>
2025-03-19 22:59:19 -0300

    feat: brew generation now work from pypi api

--- HomebrewFormula/dotcat.rb
+++ HomebrewFormula/dotcat.rb
@@ -2,8 +2,8 @@ class Dotcat < Formula
   include Language::Python::Virtualenv
 
   desc "Cat structured data , in style"
-  homepage "https://github.com/arthur-debert/dotcat"
-  url "https://files.pythonhosted.org/packages/source/d/dotcat/dotcat-0.9.6.tar.gz"
+  homepage "https://pypi.org/project/dotcat/"
+  url "https://files.pythonhosted.org/packages/61/7a/2b5ba10c4b31846f56cebbf7b761da5ec316a124d2c35c3627bc45dbfb75/dotcat-0.9.6.tar.gz"
   sha256 "24a9fcb3286fb9267443ceb3416dd4e64956641662a1154bdb78d23ec030e5b3"
   license "MIT"
 
@@ -14,16 +14,18 @@ class Dotcat < Formula
 
     # Install zsh completion
     zsh_completion.install "zsh/_dotcat" => "_dotcat"
-    # Install the helper script for ZSH completions
-    bin.install "zsh/dotcat-completion.py"
+    # Install the helper script for ZSH completions if it exists
+    if File.exist?("zsh/dotcat-completion.py")
+      bin.install "zsh/dotcat-completion.py"
+    end
   end
 
   test do
     # Add some basic tests to verify the installation
-    assert_match "dotcat v0.9.6", shell_output("#{bin}/dotcat --version")
+    assert_match "dotcat v0.9.6",
+                 shell_output("#{{bin}}/dotcat --version")
 
     # Test if the completions are installed correctly
     assert_predicate zsh_completion/"_dotcat", :exist?
-    assert_predicate bin/"dotcat-completion.py", :exist?
   end
 end
