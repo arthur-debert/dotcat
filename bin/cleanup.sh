475c9c1ec9dfb2358913268d8ff69acd1644c2d7
adebert <debert@gmail.com>
2025-06-16 15:15:22 -0300

    remove python based files

--- bin/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env shell
-# deletes all empty directories in the current directory``
-find . -type d -empty -delete
-# removes python cache files
-find . -name __pycache__ -type d -exec rm -r {} +
